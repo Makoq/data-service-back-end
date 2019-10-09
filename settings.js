018aae2351361eb27bbc3f243845e23281772bec
Makoq <913088741@qq.com>
2019-10-09 15:17:42 +0800

    10-9

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@
  * Created by kingw on 2017/6/17 10:22.
  */
 module.exports = {
-    dburl: "mongodb://127.0.0.1:27017/VgeConfigurableDataDB",
+    dburl: "mongodb://223.2.32.137:27017/VgeConfigurableDataDB",
 
 }
